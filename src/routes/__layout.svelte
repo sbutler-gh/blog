959b7e444420a44e4b0ce40d55d86e395b90ea66
sbutler-gh <sam@ysli.be>
2021-08-08 11:27:41 +0200

    Fixed footer on index for better simplicity

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,19 +2,19 @@
 	import '../app.css';
 	import Example from "$lib/Example.svelte.md";
 	import Header from "$lib/Header.svelte";
-	import Footer from "$lib/Footer.svx";
+	// import Footer from "$lib/Footer.svx";
 
-	let path;
+	// let path;
 
-	if (typeof window !== "undefined") {
-		path = window.location.pathname;
-	}
+	// if (typeof window !== "undefined") {
+	// 	path = window.location.pathname;
+	// }
 </script>
 
 <Header></Header>
 
 <slot />
-
+<!-- 
 {#if path == "/"}
 <Footer></Footer>
-{/if}
+{/if} -->
