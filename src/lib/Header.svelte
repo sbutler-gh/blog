0dd960307d1fd31a0583dd848fb9c6716caba9ad
sbutler-gh <sam@ysli.be>
2021-08-08 11:30:21 +0200

    Fixed links in Header — need to fix better, not sure why routing isn't workgin

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -16,9 +16,9 @@
 </script>
 
 <div style="display: flex">
-<h3 style="margin:0px; margin-right: 15px;"><a class="h-card p-org u-url" href="/">Sam Butler</a></h3>
-<h4 style="margin:0px; margin-right: 15px;"><a href="/ideas">Ideas</a></h4>
-<h4 style="margin:0px;"><a href="/">About</a></h4>
+<h3 style="margin:0px; margin-right: 15px;"><a class="h-card p-org u-url" href="https://sambutler.us">Sam Butler</a></h3>
+<h4 style="margin:0px; margin-right: 15px;"><a href="https://sambutler.us/ideas">Ideas</a></h4>
+<h4 style="margin:0px;"><a href="https://sambutler.us">About</a></h4>
 <div id="subscribe_div" style="margin-left: auto;">
 <div id="subscribe_form" style="display: none; position: relative;">
 {#if subscribe_open == true}
