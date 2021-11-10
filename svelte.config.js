e16982f61cdb17eafa3751eb2934f31c352b36a4
sbutler-gh <sam@ysli.be>
2021-11-10 19:35:50 +0100

    Updating config for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -5,19 +5,19 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: [".svelte", ...mdsvexConfig.extensions],
-    preprocess: [preprocess({
-        postcss: true
-    }), mdsvex(mdsvexConfig)],
+	preprocess: [
+		mdsvex(mdsvexConfig),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(), // currently the adapter does not take any options
 		target: '#svelte'
 	},
-	vite: {
-		optimizeDeps: {
-		  include: ["fastq"]			
-		}
-	  }
+	// vite: {
+	// 	optimizeDeps: {
+	// 	  include: ["fastq"]			
+	// 	}
+	//   }
 };
 
 export default config;
