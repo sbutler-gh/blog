920cc1928b8ef84174dfe8442ae1bc66a49f394d
sbutler-gh <sam@ysli.be>
2022-06-29 19:16:36 +0200

    climate justice post

---
+++ src/routes/climate-justice.svelte
@@ -0,0 +1,53 @@
+What we want is #Indigenous land sovereignty.
+
+
+
+See the work of Forrest Fleischman — landscapes managed by people are much more successful than general tree planting projects.  (https://twitter.com/ForrestFleisch1)
+
+
+
+See the perspectives of Indigenous communities around the world. (Indigenous Environmental Network, Survival International, Cultural Survival, https://climatefalsesolutions.org/)  
+
+
+
+When you remove people from the land they live on, you force communities to participate in extractive economies to survive.  Forcing entire communities away from their land, into extractive and consumptive modalities, causes far more damage and extraction than a tree planting project restores.  Not to mention, the forced evictions  of people, and violence, and worse — the most important consequences to consider.
+
+
+
+By trying to force the restoration, you end up making the problem worse then when you started.  It's a snake eating its tail.  The answer?
+
+
+
+Expanding #indigenous land sovereignty.
+
+
+
+It's messy, it's political, it's complicated, it involves people.  And yet, we know it works — and we know it's the right thing to do.
+
+
+
+It would be fun if the world's biggest problem was "increasing the pipeline of funding to nature-based projects."  We could spend all day talking to companies, getting rich off carbon offsets, building fancy digital tools, enjoying the luxuries and wealth of a new regenerative world.
+
+
+
+But as time has shown — these projects are fundamentally unproductive, and result in forced displacement, violence, biodiversity pressure, and counterproductive snake-eating-its-tail consequences along the way.
+
+
+
+#Indigenous land sovereignty is how we protect nature.  How we do that, is a much more difficult question.  (Get out the vote in Brazil this year). But at least we know it's directionally correct — and aligned with the world we want to see.
+
+
+
+Just see this week's headline — "Gas companies paying rural villagers a pittance for carbon offsets"(https://www.bloomberg.com/features/2022-carbon-offset-credits-mexico-forest-bp/, by Max de Haldevang), and explain how that "tool in the toolbox" gets us anywhere.
+
+
+
+There is an obvious need for regeneration.  Paying for it through financialization fails — and glosses over the fact, that in 2010, nations around the world agreed to be paying $200B/year to Global South countries by 2020.  It's not private markets, it's not corporate commitments, it's not easy.  But it's systems change — and it's a way to pay for restoration, in a way that supports sovereignty and builds the world we want to live in.
+
+
+
+There are other ways to pay for transitions away from extraction too, in alignment with local communities and sovereignty-building.  None of them have to do with valuing nature in $.  If you're interested in discussing or learning about those possibilities — in solidarity and mutualism and duty, rather than opportunism — leave a comment or send a DM, and glad to discuss more.
+
+
+
+#climate #sovereignty #climatejusticenow
