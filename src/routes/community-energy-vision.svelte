43d66f3e91496d4296868691c2d5584140311e4c
sbutler-gh <sam@ysli.be>
2022-07-25 14:07:01 +0200

    updated community energy page

---
+++ src/routes/community-energy-vision.svelte
@@ -0,0 +1,89 @@
+<svelte:head>
+  <title>Community Energy Vision</title>
+
+  
+  <meta name="description" content="Community energy hubs — postgrowth, less consumption, better outcomes.">
+  
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://sambutler.us/community-energy-vision/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Community Energy Vision">
+  <meta property="og:description" content="Community energy hubs — postgrowth, less consumption, better outcomes.">
+  <meta property="og:image" content="./solar_shelter.jpeg">
+  
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="sambutler.us">
+  <meta property="twitter:url" content="https://sambutler.us/community-energy-vision/">
+  <meta property="og:title" content="Community Energy Vision">
+  <meta property="og:description" content="Community energy hubs — postgrowth, less consumption, better outcomes.">
+  <meta name="twitter:image" content="./solar_shelter.jpeg">
+</svelte:head>
+
+<article class="h-entry">
+  <h1 class="p-name"><a class="u-url" href="https://sambutler.us/community-energy-vision">Community Energy Vision</a></h1>
+  <div class="e-content">     
+
+<img src="./solar_shelter.jpeg" loading="lazy" style="max-width: 100%">
+
+<p style="font-size: 13px; text-align: center"><em>Sketch of a real-life <a href="https://cebrightfutures.org/browse-projects/jefferson-park-community-solar">community solar shelter</a> in Jefferson Park, Washington. <strong>(Geekwire.com)</strong></em></p>
+
+
+<p>This morning, I read an analysis of <a href="https://sociallifeofenergy.substack.com/p/energy-of-by-and-for-the-people">community microgrid systems</a> — and the problems with implementing them in real-world contexts, which leads to a <em>50-100% failure rate of microgrids/minigrids, usually due to the overconsumption of electricity.</em></p>
+<p>Problems:</p>
+<ul>
+<li>People use more electricity than they agree to, which puts strains on the system and equipment.</li>
+<li>People make modifications to the system without consent/approval (e.g. to get more electricity, use different appliances), which also puts strains on the system and the equipment.</li>
+<li>People don&#39;t want to police each other in maintaining this microgrid electricity commons.</li>
+<li>Having a commercial entity run the microgrid may come with its own issues (although, in the spirit of Nondominium, an external impartial entity that has outcomes aligned with the community could be a good way to go forward.)</li>
+</ul>
+<p>When reading about these issues, it brought me to communicate a vision I often think about, for the future of energy in our communities.  The way it goes is this:</p>
+<ol>
+<li><p>We don&#39;t have enough resources (e.g. copper, silicon, rare metals) for every single home to have its own rooftop solar or renewable installation.  It would be nice and convenient if we did, but if we&#39;re being honest with ourselves, this seems like one more excess and overreach that we can&#39;t afford.</p>
+</li>
+<li><p>If we don&#39;t have the resource capacity to put renewable energy infrastructure on every single home, how can we make a renewable transition?</p>
+</li>
+</ol>
+<p>Answers come in the example of what places like Kokota and Puerto Rico are doing.  In Puerto Rico, people are installing renewables on <a href="https://grist.org/climate-energy/puerto-ricans-are-powering-their-own-rooftop-solar-boom/">mutual support centers</a>, which act as central locations where people can get electricity (e.g. for air conditioning, to charge devices, to get Internet, to store and prepare food) — especially during grid blackouts. (If there&#39;s a summer blackout, your AC stops working.)</p>
+
+
+<img src="./renewable_pr_center.webp" loading="lazy" style="max-width: 100%">
+
+<p style="font-size: 13px; text-align: center"><em>Renewable installation at the Mutual Support Center in Caguas, Puerto Rico. <strong>(Maria Gallucci / Canary Media, via Grist.org)</strong></em></p>
+
+<p>In Kokota, an islet in Tanzania, they have a central location where solar panels charge <a href="https://www.permaculturenews.org/2015/01/08/how-to-build-your-own-portable-microgrid/">a bank of portable batteries</a>.  When people want electricity, they can come and take one of the batteries, and take the battery home with them to power things at home.  When the battery runs out, they return it to the central solar-powered battery bank, and wait for it to recharge.</p>
+
+<img src="./batteries_kokota.jpeg" loading="lazy" style="max-width: 100%">
+
+<p style="font-size: 13px; text-align: center"><em>Solar-powered battery charging station in Kokota. <strong>(Jeff Schnurr, via Permaculturenews.org)</strong></em></p>
+
+<p>When thinking about my own community, the communal hub approach often comes to mind.  Which is simpler and more antifragile: adding solar installation to a community center, or layering a renewable microgrid over all the houses in the neighborhood?</p>
+<p>Given material constraints on resources like copper and the realities of these systems – an isolated unit is much simpler than connected units – perhaps we should be reorienting some of our paradigms away from <em>every house has its own electricity infrastructure and microgrid connection</em>, to, <strong>when you want electricity – for heat, cooling, devices, powering tools, charging, internet, any use – you go to the community hub</strong>.</p>
+<p>This approach likewise aligns with a postgrowth vision of community-centric, access-based paradigms — in contrast to individual-centric, ownership-based paradigms — and getting better at doing the things we want (e.g. heating, cooling, powering devices, working, getting Internet access) with less energy and resource consumption.</p>
+<p>A relevant context we could explore this is Germany.  As they cut back on gas from Russia, apartments will be limited to 17°C/62°F through gas heating.  As the country explores its possibilities and winter gets closer, what strategy would you choose to provide additional energy capacity?</p>
+<ul>
+<li>Implementing microgrids across ever neighborhood in Germany</li>
+<li>Implementing a renewable powered community center in every neighborhood in Germany -- where people can go when its cold, go to work with their laptop, take hot showers, cook on induction stoves, etc</li>
+<li>Anything else?</li>
+</ul>
+<p>I would choose the community approach.  I came to this perspective through actually trying to implement renewable energy in my community, and learning about how much simpler it is to have all of the infrastructure at a single location, along with a paradigm of people coming to that location to access energy.</p>
+
+  </div>
+
+<div style="display: none">
+      <p style="font-style: italic"> - <a rel="author" class="p-author h-card" href="https://sambutler.us">Sam Butler</a> (<time class="dt-published" datetime="2022-07-25 11:46:00+02:00">25<sup>th</sup> July 2022</time>)</p>
+In terms of language, we could also think this as a federated network of energy, in comparison to a centralized network (national grid) or a decentralized network (microgrids).  We shouldn't lionize federated topologies (nor should we centralized or decentralized topologies) — rather, we should evaluate them on their merits in a given context.  
+For my full, initial response to the article, read below:
+
+Perhaps the problem is the design paradigm of the microgrid.  Trying to distribute the electricicty across all of the different homes, and coordinating that wider network, is where the problems arise. Compare that to creating a renewable installation at a centralized location/hub, and the people coming to that hub for electricity usage?
+
+The latter federated design (each neighborhood having a central installation) seems to be working quite well with battery banks in (), and solar powered community centers in Puerto Rico.
+
+And when thinking about my own community, and resilience in case of blackouts, the communal hub approach often comes to mind -- which is simpler and more antifragile:  adding solar installation to a community center, or layering a renewable microgrid over all the houses in the neighborhood?
+
+Given material constraints on resources like copper and the realities of these systems -- an isolated unit is much simpler, communally and tehnically and resource-wise, than connected units -- perhaps we should be reorienting some of our paradigms away from 'every house has its own electricity infrastructure and microgrid connection', to, 'when you want electricity -- for heat, cooling, devices, powering tools, charging, internet -- you go to the community hub'?
+
+As relevant context we could explore this is Germany.  As they cut back on gas, apartments will be limited to 17C through gas heating.  As the country explores its possibilities and winter gets closer, what strategy do we think makes sense -- trying to set uo commercial or community-operated microgrids acroas ever neighborhood in Germany, or setting up a renewable powered community center in every neighborhood in Germany -- where people can go when its cold, go to work with their laptop, take hot showers, cook on induction stoves, etc?
+
+</div>
+</article>    
