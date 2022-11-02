1a582d55152af081f6058840ffd7dabed7815cc6
Sam Butler <hello@sambutler.us>
2022-11-02 14:41:48 +0100

    Add climate story

---
+++ src/routes/climate-story.svelte
@@ -0,0 +1,17 @@
+<article class="h-entry">
+    <h1 class="p-name"><a class="u-url" href="https://sambutler.us/climate-story">Climate Story — Call for Collaborators</a></h1>
+    <p style="font-style: italic">Sam Butler (November 2nd, 2022)</p>
+    <div class="e-content">   
+
+<p>I'm working on a story about climate #migration, mixing cultures, and convivial transitions &mdash; in the face of an #extraction culture, and challenges to survive.</p>
+
+<p>Do you know any folks who could be interested in imagining and working on these worlds together?</p>
+
+<p>The story may feature characters and communities from sacrifice zones. #Indigenous coastal peoples, #island nations, those dying from drought and famine in #Kenya and #Africa.</p>
+
+<p>I'm looking for collaborators who can represent and uphold these cultures + communities in the story.</p>
+
+<p>It also gets into housing and land rights, #lowtech convivial transitions, #postgrowth worlds, and #landback in the USA.</p>
+
+</div>
+</article>    
