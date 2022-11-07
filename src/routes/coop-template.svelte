b6bbaae0e901142cc3d8025114d00aebd6a21c68
Sam Butler <hello@sambutler.us>
2022-11-07 15:43:15 +0100

    add coop template document

---
+++ src/routes/coop-template.svelte
@@ -0,0 +1,82 @@
+<article class="h-entry">
+    <h1 class="p-name"><a class="u-url" href="/coop-template">Cooperative Approach to Sustainable Project Funding</a></h1>
+    <p style="font-style: italic">Sam Butler (November 7th, 2022)</p>
+    <div class="e-content">   
+<h4>A template for a cooperative approach to funding public interest projects. This should probably be adjusted to fit different project and community contexts.</h4>
+
+<p><strong>[Insert name]</strong> cooperative exists to provide <strong>[insert project/service]</strong> to everybody in the world, equitably and without profit interests.</p>
+
+<p>How does it work?</p>
+
+<p>People pay monthly dues to join the cooperative. This gives them a vote in the cooperative's operation (e.g. how to use available funding — one person, one vote) and likewise gives them access to <strong>[insert project/service]</strong>, which they would pay for otherwise.</p>
+
+<p>As the cooperative grows, the marginal cost for provisioning services decreases. 
+    
+    <p>Surplus funds can be <em>invested in the project's development</em>, <em>providing services to people free of charge</em> (in alignment with the equity mission), and <em>lowering the cost of the services for members</em>.</p>
+
+<p>Those who join the cooperative should understand it is not to create dividends or serve as an investment, and profits will not be distributed.</p>
+
+<p>Rather, the cooperative's purpose is to sustainably fund a service that we want in our lives. By working together, we can 1) improve that service (through collective funding and effort), 2) make the service more affordable (through decreasing marginal costs), and 3) equitably expand access to the service (through decreasing marginal costs).</p>
+
+<p>It's concievable, at some point in the future, that decreasing marginal costs could make the service close to free, even while it is sustainably funded! And what would be better than that &mdash; a tool/service that we enjoy and depend on in our lives, and having it close to free?</p>
+
+<p>That, in essence is the North Star of the [insert name] cooperative.  If you want to join the journey – we'll go farther together.</p>
+</div>
+</article>
+
+
+
+<!-- 
+    
+Other notes, from past writings and thoughts:
+
+    And what is better than that — a tool/service that we enjoy our lives, and having it freely available?
+
+At some point, it's concievable that that the project may not need further technical development — 
+
+The purpose of this cooperative is not to create dividends for collective, shared owners or serve as a profitable investment.
+
+Rather, it is to collectively fund a service that we want in our lives — for no reason other than our wish to use the service — 
+
+re-invested in the project's development, provide services to communities free of charge (in alignment with the project's mission), 
+
+You can join the cooperative by contributing [a flexible monthly due starting at $10], and in joining, you recieve:
+
+* A vote in the governance and priorities of the project (one person, one vote)
+
+* Access to smallweb services, which you would otherwise have to pay for (unless self-hosting)
+
+Members of the small web cooperative collectively decide how to use the project's funding (e.g. all of their member dues) towards the coop's mission, of providing small web tech to everyone in the world, equitably and without profit interests.
+
+In some cases, there may be a surplus of funds available.  In some cooperative structures, members of the coop could choose to distribute these funds among the membership.
+
+Given that small web cooperative does not have a profit interest, the cooperative will use surplus funds to provision services/usufruct (not dividends) to its membership — or dedicate that funding towards the advancement of the project.
+
+The coop's funds may typically be used to: pay for work on the project, infrastructure costs, and administrative costs.
+
+I've been thinking lately about a platform coop with a goal of providing communications as a basic server/infrastructure for everybody in the world, at as low cost as possible.  Essentially:
+
+Platform Coop
+
+^ Provisioners of Matrix Servers pay some profits to the platform coop.
+
+^^ Server maintainers pay a fair amount to server provisioners
+
+^^^ Individual server accounts pay a fair amount to server maintainers
+
+And the platform coop shares profits with member stakeholders (e.g. provisioners, maintainers, perhaps individuals), towards the outcome of providing communications access to everybody in the world for as little cost as possible.
+
+As more people join, marginal costs go down, and thus the infrastructure becomes cheaper for everybody.
+
+What GoMatrixHosting is doing makes this feasible, it just seems like their project lead wants to run a small business, and isn't interested in the coop direction.
+
+But the main maintainer/technical lead for that project is open to coop stuff.  And it's open source, so it wouldn't be a blocker in any case, but yeah — the core maintainer and the community definitely seems to vibe in the coop direction.
+
+Also, the point of introducing the server provisioners is to lower the barrier to entry for people who wish to manage their own servers — enabling non-technical people can manage their own servers too.
+
+
+Joining the cooperative gives you a vote (one-person one-vote) in the governance and priorities of the project.  
+
+You can join the cooperative by paying a monthly due.
+
+[Name of project] cooperative exists to provide [project technology]s= -->
