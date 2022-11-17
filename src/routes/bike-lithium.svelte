f3d17f1eb38f59a8e3cd3cbabf0fa8d1726aa4e0
Sam Butler <hello@sambutler.us>
2022-11-17 22:58:05 +0100

    Updated bikes transit housing picture

--- src/routes/bike-lithium.svelte
+++ src/routes/bike-lithium.svelte
@@ -2,6 +2,6 @@
     <h1 class="p-name"><a class="u-url" href="/bike-lithium">Lithium in the age of Bikes, Transit, Housing, and Justice</a></h1>
     <p style="font-style: italic">Sam Butler (November 17th, 2022)</p>
     <div class="e-content">   
-<img src="/bike_lithium_chart.png">
+<img src="/bike_transit_housing.png">
 </div>
 </article>
