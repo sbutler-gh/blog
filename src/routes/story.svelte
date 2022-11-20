525f402439977c8818f9d565a166c881efe83def
Sam Butler <hello@sambutler.us>
2022-11-20 16:15:26 +0100

    add story first draft

---
+++ src/routes/story.svelte
@@ -0,0 +1,144 @@
+<p>There used to be worlds everywhere like this.</p>
+
+<!-- <p>[Scenes of old growth forest, refuge, oasis, serenity, from regions and places around the world]</p> -->
+
+<p>And it wasn't underdeveloped.</p>
+
+<p>Being with animals in real life &mdash; watching them, in relationship.</p>
+
+<p>Face behind a screen watching them, seeing your reflection in the screen, in a panning out neoliberal dystopia.</p>
+
+<p>Show what 'development' has brought.</p>
+
+<p>Kids addicted to screens. Lifeliess worlds. Seasons out of order.</p>
+
+<p>Everhthing we know, and love, gone.</p>
+
+<p>Is this what we want?</p>
+
+<p>No.</p>
+
+<p>But how can we change?</p>
+
+<p>This in't the first crisis we've faced.</p>
+
+<p>Look at all the life that lives today, through it all.</p>
+
+<p>We found ways to thrive, survive, and live in relation through these challenges. We made mistakes, we learned, we practiced. We weaved cultures. We spun worlds.</p>
+
+<p>That is our choice today.</p>
+
+<p>Do we want this? [today]</p>
+
+<p>Or can we see the world - the many worlds around us - in new ways?</p>
+
+<p>Can we try a ritual bonfire, to honor a fellow animal in our society?</p>
+
+<p>Can we see time differently &mdash; see the past as part of our present, and our present as part of our future?</p>
+
+<p>Can we give gifts to our descendants - things they will be grateful for - just as we are grateful for the gifts our ancestors left us?</p>
+
+<p>Can we see worlds differently, today, so they can become different - in the ways that our descendants will appreciate?  Will rely on, to survive?</p>
+
+<p>Can we live every day like this, and embrace the difficult questions, that will allow us truly to see?</p>
+
+<p>Can we see the beauty and capacity in diversity?</p>
+
+<p>Can we live in relationship - with consent, responsibility, as kin - with the leafy and many-legged relatives in our worlds?</p>
+
+<p>Can we accept that life is change, fluid, motion, groundless - and can we live kinetically, in motion, without seeking ground?</p>
+
+<p>Can we recognize that there is nothing new under the sun - and 40,000 years ago, our ancestors were doing these very same things? Can we hear their songs - can we remember, sing with them, and carry on their fires?</p>
+
+<p>And like flames and fireflies lighting the path - dancing, crackling, almost extinguished, but not yet - can we illiuminate the path as we talk with our descendants, and light their path ahead, building as we walk, every day?</p>
+
+<p>Thank you, ancestors - the abused and the guided, the suffered and survived, the used and abusive, the raped and pillaged, the evangelized and conquered, the next-in-line who couldn't break the wheel, who became all the things they suffered and made the same injustices - for the fire and songs you keep alive.</p>
+
+<p>Thank you, children - for the wisdom you speak us before you're here to speak it, and gracing us with our relation and responsibility to you.</p>
+
+<p>These are new oaths, new vows, new relations, new responsibilities - but in reality, they are old, buried in the places beneath us, waiting to see the light of day again.</p>
+
+<p>And waiting to be sung by descendants to come.</p>
+
+<p>You don't need to give up everything to begin this journey, though, &quot;a rich man and a camel, live among the animasl.&quot;</p>
+
+<p>You simply need to live in conversation with your ancestors - not necessarily to act as they did, or say that they were just - but to hear and listen to what they would say about our present situation, the advice they would give us now, and honor that guidance.</p>
+
+<p>And live in conversation with our deep descendants, those we haven't met and may never, at least in this body - to listen to them and hear their wisdom on what we can do for them today.</p>
+
+<p>And make kin with the forgotten members of our worlds, listen and learn their languages and time, and re-learn our responsibilities to them, live in consent with them.</p>
+
+<!-- There used to be worlds everywhere like this.
+
+[Scenes of old growth forest, refuge, oasis, serenity, from regions and places around the world]
+
+And it wasn't underdeveloped.
+
+Being with animals in real life — watching them, in relationship, up close.
+
+Face behind a screen watching them, seeing your reflection in the screen, in a panning out neoliberal dystopia.
+
+Show what 'development' has brought.
+
+Kids addicted to screens.  Lifeliess worlds.  Seasons out of order.
+
+Everhthing we know, and love, gone.
+
+Is this whatg we want?
+
+No.
+
+but how can we change?
+
+This sin't the first crisis we've faced.
+
+Look at all the life that lives today, through it all.
+
+We found ways to thrive, survive, and live in relation through these challenges.  We made mistakes, we learned, we practiced.  We weaved cultures.  We spun worlds.
+
+That is our choice today.
+
+Do we want this? [show today]
+
+Or can we see the world - the many worlds around us - in new ways?
+
+Can we try a ritual bonfire, to honor a fellow animal nation in our place?
+
+Can we see time differently — see the past as part of our present, and our present as part of our future?
+
+Can we give gifts to our descendants - things they will be grateful for - just as we are grateful for the gifts our ancestors left us?
+
+Can we see worlds differently, today, so they can become different - in the ways that our descendants will appreciate, and rely on, to survive?
+
+Can we live every day like this, and embrace the difficult questions, that will allow us truly to see?
+
+Can we see the beauty and capacity in diversity?
+
+Can we live in relationship - with consent, responsibility, as kin - with the leafy and fibrous and many-legged relatives in our worlds?
+
+Can we accept that life is change, fluid, motion, groundless - and can we live kinetically, in motion, with power, without seeking ground?
+
+Can we recognize that there is nothing new under the sun - and 40,000 years ago, our ancestors were doing these very same things?
+Can we hear their songs - can we remember, can we join them in singing, and carry on their fires?
+
+And like many flames and fireflies lighting the path - dancing, crackling, almost extinguished, but not yet - can we illiuminate the path as we talk with our descendants, and light their path ahead, building as we walk, every day?
+
+Thank you, ancestors - the abused and the guided, the suffered and survived, the used and abusive, the raped and pillaged, the evangelized and conquered, the next-in-line who couldn't break the wheel, who became all the things they suffered, and made the same injustices - for the fire and songs you keep alive.
+
+Thank you, chilren - for the wisdom you speak us, before you're here to speak it, and gracing us with our relation and responsibility to you.
+
+These are new oaths, new vows, new relations, new responsibilities - but in reality, they are old, buried in the ground beneath us, waiting to see the light of day again.
+
+And waiting to be sung by descendants to come.
+
+You don't need to give up everything to begin this journey, though, "a rich man and a camel, live among the animasl."
+
+You simply need to live in conversation with your ancestors - not necessarily to act as they did, or say that they were just - but to hear and listen to what they would say about our present situation, the advice they would give us now, and honor that wisdom.
+
+And live in conversation with our deep descendants, those we haven't met and may never know, at least in this body, to listen to them and hear their wisdom on what we can do for them today.
+
+And make kin with the forgotten members of our worlds, listen and learn their languages and time, and re-learn our responsibilities to them, live in consent with them.
+
+<p>We can start listening to trees, and going on walks to reunite with those - fibrous, four-legged, winged, two-legged - who we have lost touch with, in the spirit of Nanabush in the Nishnaabeg tradition.</p>
+
+-->
