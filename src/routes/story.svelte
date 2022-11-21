565d1f3eeab5f03fdc23618456535491f50b245f
Sam Butler <hello@sambutler.us>
2022-11-21 13:00:15 +0100

    Updated story, with recognition and gratitude

--- src/routes/story.svelte
+++ src/routes/story.svelte
@@ -1,8 +1,14 @@
+<!-- <em>This is the beginning of a story about changing the way we see our worlds, changing our practices and values, changing our material and spiritual realities — because the way we see the world now is not working, and we must find (and re-discover) other ways of seeing, thinking, feeling, connecting, and living.</em> -->
+
+<p><em>This is influenced by the lessons, stories, and perspectives of Leanne Betasamosake Simpson, Lyla June, Kyle Whyte, Nick Estes, and many other Indigenous writers and scholars.  It's also influenced by the work of Chris Baulmann and Alexander Baumann, in recognizing the fundamental contradiction of transcending growth and private property (where, having a place to live requires feeding the growth machine.) And from reflecting on these perspectives and lessons in my own life.</em>
+    
+<p><em>This began while walking in a refuge of an old world, tucked beisde a gravel parking lot and a swimming pool.  Muddy hills, tall old trees, clean air.   Deep enough inside, you can lose the sound of gas motors on the weekend.  You can see that this world used to be everywhere.  This world used to be home.</em></p>
+
 <p>There used to be worlds everywhere like this.</p>
 
 <!-- <p>[Scenes of old growth forest, refuge, oasis, serenity, from regions and places around the world]</p> -->
 
-<p>And it wasn't underdeveloped.</p>
+<p>And they weren't underdeveloped.</p>
 
 <p>Being with animals in real life &mdash; watching them, in relationship.</p>
 
@@ -10,9 +16,9 @@
 
 <p>Show what 'development' has brought.</p>
 
-<p>Kids addicted to screens. Lifeliess worlds. Seasons out of order.</p>
+<p>Kids addicted to screens. Lifeless worlds. Seasons out of order.</p>
 
-<p>Everhthing we know, and love, gone.</p>
+<p>Everything we know and love, gone.</p>
 
 <p>Is this what we want?</p>
 
@@ -20,7 +26,7 @@
 
 <p>But how can we change?</p>
 
-<p>This in't the first crisis we've faced.</p>
+<p>This isn't the first crisis we've faced.</p>
 
 <p>Look at all the life that lives today, through it all.</p>
 
@@ -30,43 +36,40 @@
 
 <p>Do we want this? [today]</p>
 
-<p>Or can we see the world - the many worlds around us - in new ways?</p>
+<p>Or can we see the worlds around us in new ways?</p>
 
-<p>Can we try a ritual bonfire, to honor a fellow animal in our society?</p>
+<p>Can we try a ritual fire, to honor a more-than-human relative in our society?</p>
 
-<p>Can we see time differently &mdash; see the past as part of our present, and our present as part of our future?</p>
+<p>Can we see time differently - the past as part of our present, and our present as part of our future?</p>
 
 <p>Can we give gifts to our descendants - things they will be grateful for - just as we are grateful for the gifts our ancestors left us?</p>
 
-<p>Can we see worlds differently, today, so they can become different - in the ways that our descendants will appreciate?  Will rely on, to survive?</p>
+<p>Can we see worlds differently, today, so they can become different - in the ways that our descendants will appreciate?  In ways they will rely on, and find joy in?</p>
 
-<p>Can we live every day like this, and embrace the difficult questions, that will allow us truly to see?</p>
+<p>Can we live every day like this, and embrace the difficult questions that allow us to see?</p>
 
 <p>Can we see the beauty and capacity in diversity?</p>
 
-<p>Can we live in relationship - with consent, responsibility, as kin - with the leafy and many-legged relatives in our worlds?</p>
+<p>Can we live in relationship - in consent, responsibility, and kinship - with the leafy and many-legged relatives in our worlds?</p>
 
-<p>Can we accept that life is change, fluid, motion, groundless - and can we live kinetically, in motion, without seeking ground?</p>
+<p>Can we accept that life is change, fluid, motion, groundless - can we live kinetically, in motion, without seeking ground?</p>
 
-<p>Can we recognize that there is nothing new under the sun - and 40,000 years ago, our ancestors were doing these very same things? Can we hear their songs - can we remember, sing with them, and carry on their fires?</p>
+<p>Can we recognize that there is nothing new under the sun - and 40,000 years ago, our ancestors were doing these very same things? Can we hear their songs?  Can we remember, and sing with them? Can we carry on their fires?</p>
 
-<p>And like flames and fireflies lighting the path - dancing, crackling, almost extinguished, but not yet - can we illiuminate the path as we talk with our descendants, and light their path ahead, building as we walk, every day?</p>
+<p>And like flames and fireflies lighting the path - dancing, crackling, almost extinguished, but not yet - can we talk with our descendants and light their path ahead, building as we walk, every day?</p>
 
-<p>Thank you, ancestors - the abused and the guided, the suffered and survived, the used and abusive, the raped and pillaged, the evangelized and conquered, the next-in-line who couldn't break the wheel, who became all the things they suffered and made the same injustices - for the fire and songs you keep alive.</p>
+<p>These are new oaths, new vows, new relations, new responsibilities.</p>
 
-<p>Thank you, children - for the wisdom you speak us before you're here to speak it, and gracing us with our relation and responsibility to you.</p>
-
-<p>These are new oaths, new vows, new relations, new responsibilities - but in reality, they are old, buried in the places beneath us, waiting to see the light of day again.</p>
+<p>In reality, they are old - buried in the places beneath us, waiting to see the light of day again.</p>
 
 <p>And waiting to be sung by descendants to come.</p>
 
-<p>You don't need to give up everything to begin this journey, though, &quot;a rich man and a camel, live among the animasl.&quot;</p>
+<p>You don't need to give up everything to begin this journey.
+    
+    <p>(Though, if you believe in Jesus, he did say, "<em>Sell all that you own and distribute the money to the poor ... then come, follow me</em>", and 
+    "<em>Look at the birds of the air: they neither sow nor reap nor gather into barns, and yet your heavenly Father feeds them.</em>", and "<em>no one claimed private ownership of any possessions, but everything they owned was held in common</em>.")</p>
 
-<p>You simply need to live in conversation with your ancestors - not necessarily to act as they did, or say that they were just - but to hear and listen to what they would say about our present situation, the advice they would give us now, and honor that guidance.</p>
-
-<p>And live in conversation with our deep descendants, those we haven't met and may never, at least in this body - to listen to them and hear their wisdom on what we can do for them today.</p>
-
-<p>And make kin with the forgotten members of our worlds, listen and learn their languages and time, and re-learn our responsibilities to them, live in consent with them.</p>
+<p>You simply need to begin.</p>
 
 <!-- There used to be worlds everywhere like this.
 
@@ -123,6 +126,10 @@ Can we hear their songs - can we remember, can we join them in singing, and carr
 
 And like many flames and fireflies lighting the path - dancing, crackling, almost extinguished, but not yet - can we illiuminate the path as we talk with our descendants, and light their path ahead, building as we walk, every day?
 
+<p>Thank you, ancestors - the abused and the guided, the suffered and survived, the used and abusive, the raped and pillaged, the evangelized and conquered, the next-in-line who couldn't break the wheel, who became all the things they suffered and made the same injustices - for the fire and songs you keep alive.</p>
+
+<p>Thank you, children - for the wisdom you speak us before you're here to speak it, and gracing us with our relation and responsibility to you.</p>
+
 Thank you, ancestors - the abused and the guided, the suffered and survived, the used and abusive, the raped and pillaged, the evangelized and conquered, the next-in-line who couldn't break the wheel, who became all the things they suffered, and made the same injustices - for the fire and songs you keep alive.
 
 Thank you, chilren - for the wisdom you speak us, before you're here to speak it, and gracing us with our relation and responsibility to you.
@@ -140,5 +147,13 @@ And live in conversation with our deep descendants, those we haven't met and may
 And make kin with the forgotten members of our worlds, listen and learn their languages and time, and re-learn our responsibilities to them, live in consent with them.
 
 <p>We can start listening to trees, and going on walks to reunite with those - fibrous, four-legged, winged, two-legged - who we have lost touch with, in the spirit of Nanabush in the Nishnaabeg tradition.</p>
+
+<p>You simply need to live in conversation with your ancestors - not necessarily to act as they did, or say that they were just - but to hear and listen to what they would say about our present situation, the advice they would give us now, and honor that guidance.</p>
+
+<p>And live in conversation with our deep descendants, those we haven't met and may never, at least in this body - to listen to them and hear their wisdom on what we can do for them today.</p>
+
+<p>And make kin with the forgotten members of our worlds, listen and learn their languages and time, and re-learn our responsibilities to them, live in consent with them.</p>
+
+    <em>It is easier for a camel to go through the eye of a needle...</em>, and, <em>Look at the birds of the air: they neither sow nor reap nor gather into barns, and yet your heavenly Father feeds them.</em> and <em>Now the whole group of those who believed were of one heart and soul, and no one claimed private ownership of any possessions, but everything they owned was held in common. With great power the apostles gave their testimony to the resurrection of the Lord Jesus, and great grace was upon them all. There was not a needy person among them, for as many as owned lands or houses sold them and brought the proceeds of what was sold. They laid it at the apostles’ feet, and it was distributed to each as any had need. There was a Levite, a native of Cyprus, Joseph, to whom the apostles gave the name Barnabas (which means “son of encouragement”). He sold a field that belonged to him, then brought the money, and laid it at the apostles’ feet</em> and <em>When Jesus heard this, he said to him, ‘There is still one thing lacking. Sell all that you own and distribute the money to the poor, and you will have treasure in heaven; then come, follow me</em>.</p>
 
 -->
