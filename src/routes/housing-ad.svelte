ff9f42f2af386c0efd33d41537e2cc5915f2aff1
Sam Butler <hello@sambutler.us>
2022-11-23 22:57:15 +0100

    post housing ad documentation first version

---
+++ src/routes/housing-ad.svelte
@@ -0,0 +1,183 @@
+COLD OPEN
+
+You have a choice:
+
+Continue living normally, business as usual.  Or give up your home, and get to live in it for the rest of your life for free - with roomates.
+
+What do you choose?
+
+CUT
+
+Here's the situation:
+
+We have to massively scale down our economies.  We have a responsibility to, as 10% of our planet's human socities — cultures and communities spanning about 800 million people across the Global North — are destroying worlds, across our world.
+
+We're already at 1.5C of locked-in global warming.  We see now, that this is genocide.  Land is falling into the ground under Indigenous nations.  Places that people and more-than-human people have called home for millenia, are underwater, deserted, hollowed, and burned to nothing.  Don't look away.  Hold this.  For the past 500 years, industrial countires have been destroying worlds.  Places, cultures, communities, lands, relationships — worlds, that people called home.  This has not been through fighting or conquest or merit.  This has been through broken treaties, exploitation, and a lack of responsibilities, through and through.
+
+And to this day, we are destroying worlds in the same ways — faster than ever.
+
+We are already at 1.5 degrees of warming — which, on land, translates to the 5 degrees of warming that we've really been feeling (15 in farenheit.  1.5C is a global temperature average — on land, temperature incresases are 3x higher.)
+
+At the same time, increasing carbon dioxide levels in the air are already leading to plant malnutrition.  If you remember from school, plants require light and carbon dioxide to grow.  But what happens when you increase the amount of carbon dioxide in the recipe — say, twice as much CO2, compared to fifty years ago?  The plants produce less nutrients.  Less minerals and proteins, which are essential.
+
+This will only increase as more carbon dioxide enters the atmosphere.  And in the meantime, it may be one of the causes of insect die-off — because insects can't get enough nutrients, from plants and food sources that are already rapidly dissapearing anyway.
+
+If insects can't get food, we won't either.
+
+However, as we decarbonize and clean up the air, temperatures will get hotter — because there will be fewer aerosols in the air.  We already saw this effect occur during lockdowns in 2020, when decreasing pollution and aerosols increased temperatures in China.  We don't have an alternative — food and nutrition as we know it won't exist if we don't decarbonize.  And yet, as we decarbonize, the world will get hotter.
+
+Where does that leave us?
+
+The IPCC scenarios are bullshit.  It's 2022, and we're already at 1.5C.  We're already at tipping points, which weren't supposed to hpapen for decades.  It's now.
+
+Transitioning our economies to renewables won't put the nutrition back in our plants or address the locked-in warming we already have.  And more, only rarely, has there even been a transition — as we deploy renewables, we're also building more fossil fuel infrastructure.  Which is killing our plants, and killing our atmosphere, and killing us.
+
+All of that, to continue feeding a growth economy.  To continue feeding business as usual.  To get paid to keep the gas on under the hot tub — as worlds evaporate around us, every day.
+
+And that is the paradox.
+
+How can we scale down the economy — to rapidly decarbonize, so our plants and insects and relatives and worlds can all live — when in the very societies that are driving the problem, you are required to participate in the economy in order to live?
+
+How can we scale down the economy, if you must participate in the economy to have a place to live?
+
+To pay for a place to live?  To pay for a place to call home?
+
+It's a Gordian knot.  And here's one approach to cutting it.
+
+You give people a choice.
+
+You can go on living your life, business as usual.  Or you can give up your home — to a municipality, to a public authority — and go on living their for free, for the rest of your life, with roomates.
+
+What does that mean?
+
+Imagine it.
+
+What if you knew you could live the rest of your life in your home, for free?
+
+What would you do?
+
+First, you'd get used to new social arrangements.  A single-family home can be retrofitted and converted, to be a home for more people, while still giving people privacy — and adding, community, resilience, and something beyond the isolation and melancholoy of modern life.
+
+Next, you'd probably ask yourself — if I'm not paying rent or a mortgage, what is my money good for?
+
+For food.  For electricity.  For heat.  For Internet.  For tools and services you depend on.
+
+And you'd maybe realize, that the way the world is headed, money won't be able to buy you those things anyway.
+
+So instead of going to job — and all the energy spent in commuting, powering the office, powering the work — you could go outside, and re-learn how to survive in a changing world.  Learn how to grow food.  Learn how to produce electricity, and heat, from the sun and wind.  Learn how to build grassroots Internet networks.  Learn how to make, borrow, and find the tools and help you need.  And do it in a community.  Do it as community.
+
+And now we're consuming less.  We're using drastically less energy.  We're growing food and making things locally.  We're producing significantly less carbon, pollution, and waste.  With nowhere to go, we're walking and riding bikes.  With nothing to do, except survive, we're spending more time together — and using less, of everything.  And life is so much more.
+
+It's not utopia.  It's struggle.  It's learning.  It's failing.  It's experimenting.  It's re-learning how to live on the land and live in a place, with values and cultures that have been dormant for many, many years.  It's risking, it's trying, it's failing, it's hunger, it's cold, it's heat, it's thriving, it's bugs, it's dirt, it's ticks, it's tall grass, it's running water, it's fungal infections, and a climate that continues to change, and all kinds of new challenges that make it more challenging to survive.
+
+And yet, that is the way.  You may like it.  You may not.  (In that case, keep living alone, and see how far that gets you.) These are the worlds we're moving towards.  And the more quickly we move there, the more worlds we have to inhabit.
+
+Now, you may be asking — how would people just stop paying rent?  Just stop paying mortgages?  How would that work?
+
+There's a number of ways that could happen.  Public authorities, municipalities, governments could buy up housing debt and "municipalize" the housing.  They could buy up housing debt at discounts, and cancel it, like cities in America have started doing with medical debt.  And of course, they can also use the same tools they've used to destroy neighborhoods, build highway expansions, and operate for the past 100 years — eminent domain, executive authority, all that stuff.
+
+Importantly, though, it's a choice.  It is opt-in.  It's consent.  If you don't want to participate, you don't have to.
+
+As housing becomes public, it would be managed by the municipality — or maybe, community-led political processes.
+
+Many people continue living in the same homes they always have, keeping neighborhoods and social relations in tact.  Many new people would join them, living in those same homes, converted from single-family to multi-person and multi-family units.
+
+Since people don't need to pay for a home or a place to live or the protection of not being homeless, demand for housing drops.
+
+And in the meantime, you who chose to keep your properties, you have the chance to opt-in anytime.  As you see the world changing around them — a new paradigm, where people don't need to pay for a place to live, and can instead focus on surviving and systems and community —you can reevaluate. Do you want to keep paying your mortgage, for title to a property and the right to live alone and maybe banking on the fact that all of this will be undone and we'll return to capitalism and you'll be sitting on a gold mine?
+
+Or do you want to join the community, learn to survive, live for free, and meet some new roomates?
+
+And now, the cost for a public authority / municipality  / community to absorb that property costs significantly less — because, in a world where people can live in a home for free, the monetary value of a home is worth significantly less.
+
+And that enables the community to absorb more homes, and provide more housing, which furhter lowers the monetary value of the still private-properties, and makes it easier to absorb those homes.  It's a race to the bottom of private property valuations.
+
+And what are we left with?  Housing, where people live.  Communities, where people learn to survive together.  Places, that we re-unite with.  Ways of life, that we re-learn.  People, friends, relatives, of all matter and constitutions, that we reacquaint with.  Politics, that come out of these transitions.  Cultures, that come out of these transitions.  Education, that comes out of these transitions.
+
+We're now pulling more carbon out of the air then we're putting in — not through machines, but through plants and gardens everywhere we can see.
+
+We've significantly reduced the energy we're putting into the atmosphere.  So while it's still getting hotter, we've started addressing the root problem — and on the ground, we are dealing with the heat directly, like by putting mirrors up on our homes and over our gardens.
+
+And that's life now.  Those are our new worlds.  It's nothing new.  This is how our parents and grandparents lived, 80 years ago.  This is how cultures around the world live, to this day.  This is similar to the change that Cuba underwent in the 90s — from one of the most energy-intensive nations in the Americas, to having better life expectancy than the U.S., even amidst an embargo and while using 8x less energy.
+
+And in these new worlds — where, in the spirit of the Bible, "everything they had they had in common and shared together" — perhaps we have a chance to honor treaties, and uphold lost responsibilities, and address 500 years of dispossession and genocide.  Perhaps this is LandBack.  All the way.
+
+CUT
+
+Where do we go from here?
+
+It starts with seeing.  The paradigm.
+
+Maybe some mayor or city councilmember or student sees this, and goes to pitch it at town hall.  And they figure out how to make it work — how to absorb the houses, how to offer people the choice, with consent it.
+
+And they try it.  And it works.
+
+And people are starting to live in these worlds.  And it starts to spread.  And we are scaling down.  And we are re-learning how to live.
+
+And one day, it comes to you, and you have a choice.
+
+Business as usual?  Or live for free, and get some roomates?
+
+
+____________
+
+Maybe some mayor or city councilmember or student goes and pitches it at town hall.  They figure out how to acquire the debt, how to absorb some houses, how to offer the choice, in consent.
+
+And more places adopt it.
+
+Considering this.  Imagining.  The paradigm.  The zeitgeist.  The overton window.
+
+Maybe some small town or big village 
+
+
+
+And in these new worlds — where, in the spirit of the Bible, ""
+
+
+
+
+And as more people choose to participate and municipalize their housing
+
+
+
+
+
+As housing becomes public, it would presumably be stewarded by the municipality — or maybe, community-rooted political processes.
+
+Importantly, this is a choice.  It is opt-in.  If you don't want to participate, you don't have to.
+
+And then, as housing becomes public — people can keep living in their homes, as those homes are changed to become homes for more people — demand for housing drops.  The monetary value drops, because people don't need to pay for it anymore.
+
+And at that point, you can choose again.  Keep paying your mortgage, for title to a property and the right to live alone, in a community where everybody has the chance to live for free?  Or join the community — and meet some new roomates?
+
+
+
+So basically, housing becomes public.  
+
+
+
+
+It's not utopia.  It's learning, it's failing, it's experimenting, it's taking risks that we must take— all in a climate that continues to change, and get more challenging to survive.
+
+You may like how that sounds.  You may not like how that sounds.  It doesn't matter so much.  That is the future, if any, we will be living.
+
+
+
+What does that mean?
+
+
+
+And the IPCC reports are bullshit.  It's 2022, and we're already at 1.5C.  The artic is already at risk of melting.  This wasn't supposed to happen for decades.  It's happening now.
+
+Transitioning everything to renewables won't make plants more nutritious, address locked-in temperature heating already
+
+This is not a question.  As we decarbonize, the world will get hotter.
+
+As we decarbonize and clean up th
+
+
+ animals, insects, and entire worlds to live.
+
+If we don't lower the amount of carbon in the atmosphere, plants will become less and less nutritious — for humans, for animals, insects, 
+
+But what happens when you add too much light into the recipe?  The plants produce less nutrients, and
