1726e8fb5a036180ddd5d83506c8a9f662df567d
Sam Butler <hello@sambutler.us>
2022-11-25 20:38:33 +0100

    update covid thing

--- src/routes/covid.svelte
+++ src/routes/covid.svelte
@@ -1,3 +1,254 @@
+When you get COVID (SARS-Cov-2), you don't get sick because of the virus — you get sick because your body over-reacts to seeing the virus for the first time, and sends your immune system into over-drive.
+
+That infection doesn't give you immunity — it just means that your body has seen the virus before.  That's why the next infection feels milder: because your body's own immune reaction is less intense.
+
+(How many times have you gotten the common cold?  Have you ever gotten immune to the common cold?)
+
+This has nothing to do with what the virus is doing inside your body.
+
+What the virus *is* doing, is:
+
+• Attacking your brain cells, leading to conditions similar to Alzheimers ¹ ² 
+
+• Attacking your veins and vascular systems, leading to heart disease, stroke, blood clots, gum disease, and other cardiovascular conditions — including erectile dysfunction! (how you think the blood gets up there!) ³ ⁴
+
+• Attacking your lungs, leaving them in worse condition than chronic smokers, and reducing the lung size of newborn babies ⁵ ⁶
+
+• Attacking your body's immune system, making you more susceptible to everything else, including organ failures, autoimmune disorders, and cancer ⁷ ⁸ ⁹
+
+Getting COVID does not make you safer.  It means your body is less likely to over-react to the virus, while the virus stays in your body and continues attacking it long-term.  There is no immunity — and this is just as true for children and infants, who are experiencing more extreme illnesses, despite many of them being born after lockdowns ended. ¹⁰
+
+(Why do you think Republicans, who took such few measures to prevent infection, are dying so much more than Democrats, as compared to before the pandemic? ¹¹)
+
+There is only avoiding infection, as best as we can.
+
+
+What does all of this mean?
+
+Bringing back masks in work, school, and social settings.  Avoiding indoor gatherings.  Air circulation, with open windows and air filters.  Expanding PCR testing (because at-home over-the-counter tests often fail to detect new strains.)  Doing more things outside — on walks, in parks, in yards, as our societies have done forever.  This is all compatible with a free and open society, as we see in countries that normalized masks after the first SARS pandemic.  A short time ago, masks were normalized in many of our own countries!
+
+https://twitter.com/drclairetaylor/status/1538584972084359168
+
+
+Otherwise, this is the future that some people are counting on — and have experienced already:
+
+https://twitter.com/danaparish/status/1542322893723357184
+
+https://twitter.com/yanzhonghuang/status/1586537964267798530
+
+SARS-CoV-2 is closer to HIV than anything the media or government is telling us.  It’s shit, but this is what we know.  None of the researchers sharing this information are calling for more vaccines (which help somewhat, but not enough) or anything that would give them a conflict of interest.
+
+It’s just about avoiding situations that will risk infection, and protecting ourselves the best we can.
+
+¹ https://neurosciencenews.com/post-covid-brain-21904
+
+
+² https://twitter.com/AndrewEwing11/status/1586500342841475072
+
+
+³https://theconversation.com/even-mild-covid-raises-the-chance-of-heart-attack-and-stroke-what-to-know-about-the-risks-ahead-190552
+
+
+⁴ https://twitter.com/drclairetaylor/status/1534659666076131333
+
+
+⁵ https://twitter.com/DrEricDing/status/1350612168211910663
+
+⁶ https://twitter.com/fitterhappierAJ/status/1593673625814433793
+
+⁷https://medicine.wustl.edu/news/repeat-covid-19-infections-increase-risk-of-organ-failure-death/
+
+
+⁸ https://twitter.com/yaneerbaryam/status/1595759029363646464
+
+⁹ https://twitter.com/denise_dewald/status/1586773303582130176
+
+
+¹⁰  https://twitter.com/TRyanGregory/status/1595896241594183681
+
+
+¹¹ https://twitter.com/dcpetterson/status/1578057148029976587
+
+
+
+
+
+
+
+
+
+
+
+
+Archived text:
+
+Rought draft:
+
+
+
+When you get COVID (SARS-Cov-2), you don't get sick because of the virus — you get sick because your body over-reacts to seeing the virus for the first time, and sends your immune system into over-drive.
+
+That infection doesn't give you immunity — it just means that your body has seen the virus before.  That's why the next infection feels milder: because your body's own immune reaction is less intense.
+
+(How many times have you gotten the common cold?  Have you ever gotten immune to the common cold?)
+
+This has nothing to do with what the virus is doing inside your body.
+
+What the virus *is* doing, is:
+
+* Attacking your brain cells, leading to conditions similar to Alzheimers ¹ ² 
+
+* Attacking your veins and vascular systems, leading to heart disease, stroke, blood clots, gum disease, and other cardiovascular conditions — including erectile dysfunction! (how you think the blood gets up there!) ³ ⁴
+
+* Attacking your lungs, leaving them in worse condition than chronic smokers ⁵
+
+* Attacking your bones, leading to bone loss and missing teeth ⁶
+
+* Attacking your body's immune system, making you more susceptible to everything else, including organ failure and cancer ⁷ ⁸
+
+Getting COVID does not make you safer.  It means your body is less likely to over-react to the virus, while the virus stays in your body and continues attacking it long-term.  There is no immunity.
+
+(Why do you think Republicans, who took such few measures to prevent infection, are dying so much more than Democrats, as compared to before the pandemic? ⁹)
+
+There is only avoiding infection, as best as we can.
+
+
+What does all of this mean?
+
+Bringing back masks in work, school, and social settings.  Avoiding indoor gatherings.  Air circulation, by opening windows and air filters.  Expanding PCR testing (because at-home over-the-counter tests often fail to detect new strains.)  Doing more things outside — on walks, in parks, in yards, as our societies have done forever.  This is all compatible with a free and open society, as we see in countries that normalized masks after the first SARS pandemic.  A short time ago, masks were normalized in many of our own countries!
+
+https://twitter.com/drclairetaylor/status/1538584972084359168
+
+
+Otherwise, this is the future that some people are counting on — and have experienced already:
+
+https://twitter.com/danaparish/status/1542322893723357184
+
+https://twitter.com/yanzhonghuang/status/1586537964267798530
+
+SARS-CoV-2 is closer to HIV than anything the media or government is telling us.  It’s shit, but this is what we know.  None of the researchers sharing this information are calling for more vaccines (which help somewhat, but not enough) or anything that would give them a conflict of interest.
+
+
+¹ https://neurosciencenews.com/post-covid-brain-21904
+
+
+² https://twitter.com/AndrewEwing11/status/1586500342841475072
+
+
+³https://theconversation.com/even-mild-covid-raises-the-chance-of-heart-attack-and-stroke-what-to-know-about-the-risks-ahead-190552
+
+
+⁴ https://twitter.com/drclairetaylor/status/1534659666076131333
+
+
+⁵ https://twitter.com/DrEricDing/status/1350612168211910663
+
+⁶ https://medicine.iu.edu/blogs/research-updates/iu-researchers-study-long-term-effects-of-covid-19-on-bone-growth
+
+⁷  https://medicine.wustl.edu/news/repeat-covid-19-infections-increase-risk-of-organ-failure-death/
+
+
+⁸ https://twitter.com/denise_dewald/status/1586773303582130176
+
+⁹ https://twitter.com/dcpetterson/status/1578057148029976587
+
+¹⁰ 
+¹¹ 
+¹² 
+¹³ 
+¹⁴ 
+¹⁵
+
+
+
+
+
+Other threads:
+
+
+https://twitter.com/fitterhappierAJ/status/1595878927243051008
+
+https://twitter.com/alexmeshkin/status/1596114095048040448
+
+https://twitter.com/_ppmv/status/1588304732132900866
+
+https://twitter.com/maolesen/status/1471188829302317056
+
+https://twitter.com/_ppmv/status/1588087770156769281
+
+https://twitter.com/_ppmv/status/1588081444676583426
+
+https://twitter.com/BinitaKane/status/1596049390862761984
+
+
+
+Link inline version:
+
+When you get COVID (SARS-Cov-2), you don't get sick because of the virus — you get sick because your body over-reacts to seeing the virus for the first time, and sends your immune system into over-drive.
+
+That infection doesn't give you immunity — it just means that your body has seen the virus before.  That's why the next infection feels milder: because your body's own immune reaction is less intense.
+
+(How many times have you gotten the common cold?  Have you ever gotten immune to the common cold?)
+
+This has nothing to do with what the virus is doing inside your body.
+
+What the virus *is* doing, is:
+
+* Attacking your brain cells, leading to conditions similar to early on-set Alzheimers
+
+Article: https://neurosciencenews.com/post-covid-brain-21904/, Thread of Research Papers: https://twitter.com/AndrewEwing11/status/1586500342841475072
+
+* Attacking your veins and vascular systems, leading to heart disease, strokes, blood clots, and other cardiovascular diseases — including erectile dysfunction (how you think the blood gets in there!)
+
+Paper: https://theconversation.com/even-mild-covid-raises-the-chance-of-heart-attack-and-stroke-what-to-know-about-the-risks-ahead-190552
+
+Thread and papers: https://twitter.com/drclairetaylor/status/1534659666076131333
+
+* Attacking your lungs, leaving your lungs in worse condition than chronic smokers
+
+https://twitter.com/DrEricDing/status/1350612168211910663
+
+* Attacking your body's immune system, making you more susceptible to every other condition, including future COVID infections and cancer risks (by suppressing a gene that's responsible for helping mitigated cancer)
+
+Getting infected does not make you safer.  It means your body is less likely to *react* to the virus, while the virus stays in your body and continues attacking it long-term.  There is no immunity.
+
+There is only avoiding infection, as best as we can.
+
+What does this mean?
+
+Bringing back masks in work, school, and social settings.  Avoiding indoor gatherings.  Air circulation, by opening windows and air filters.  Expanding PCR testing (because at-home over-the-counter tests are often failing to catch new strains.)  Doing more things outside — on walks, in parks, in yards (even in the winter), as our ancestors have done for all of time.  This is all compatible with a free and thriving society, as we see in countries that normalized masks after the first SARS pandemic.
+
+https://twitter.com/drclairetaylor/status/1538584972084359168
+
+
+
+
+Otherwise, this is the future that some people are counting on:
+
+https://twitter.com/danaparish/status/1542322893723357184
+
+https://twitter.com/yanzhonghuang/status/1586537964267798530
+
+
+
+
+
+
+https://twitter.com/fitterhappierAJ/status/1595878927243051008
+
+https://twitter.com/alexmeshkin/status/1596114095048040448
+
+https://twitter.com/_ppmv/status/1588304732132900866
+
+https://twitter.com/maolesen/status/1471188829302317056
+
+https://twitter.com/_ppmv/status/1588087770156769281
+
+https://twitter.com/_ppmv/status/1588081444676583426
+
+https://twitter.com/BinitaKane/status/1596049390862761984
+
+
 Thinking about how to communicate covid.
 
 When you get COVID, you don't get sick because of the virus — you get sick because your body over-reacts to seeing the virus for the first time, and sends your immune system into over-drive.
