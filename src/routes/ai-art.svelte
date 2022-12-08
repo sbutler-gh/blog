c65b3d6834f8f2637a19890a07cbc57df165b70b
Sam Butler <hello@sambutler.us>
2022-12-08 22:56:40 +0100

    update aiart story

--- src/routes/ai-art.svelte
+++ src/routes/ai-art.svelte
@@ -2,11 +2,30 @@
     <h1 class="p-name"><a class="u-url" href="/ai-art">The Problem of AI Art</a></h1>
     <p style="font-style: italic">Sam Butler (December 8th, 2022)</p>
     <div class="e-content">
+        When subsistence lifestyles re-emerge, perhaps people who support cultures through art will be supported through their cultures in kind, and arts will re-emerge as cultural tools (as they've often been) rather than commodities. <br>
+
+Then, #AiArt questions may get more practical... <br>
+
+Does an #art and #practice like this support our local culture?  Is it a good use of energy?  Do we like it?  Is it a practice that we want to pass on?  What about for accessibility and equity?  What about in different contexts? <br>
+
+
+For now, the most important question seems to be, "is it a good use of energy?" <br>
+
+That may always be the question, in an era of #ClimateCrisis and #Degrowth. <br>
+
+And hopefully our practices around #AiArt (and, well, everything) will answer that question wisely. <br>
+
+<!-- Feel free to follow/subscribe so I can share work and ideas with more people -- through words, interactive tools, stories, collectives, organizing, solidarity, minimal computing, and more!  (And all things #postgrowth, #degrowth, #climate) 4/
+
+https://sambutler.us
+
+
+
         When subsistence lifestyles resurge, artists won't need to "pay for a living." People who support their cultures through art (I hypothesize) will be supported by their communities in kind.  That's what many our pasts show us, at least.<br>
 
         Then, questions of #AiArt will be more practical.  Is it beneficial to the survival and progress of our culture?  Is it a good use of energy?  Do we like it?  Is it a practice that we want to pass on?  What about for accessibility and equity?  What about for different contexts?<br>
 
-        Today, the main question seems to be, "is it a good use of energy."  That is a complicated and important question.  There are ways AI can probably use less energy than current practices (e.g. one prompt compared to 10 Google searches, or one prompt compared to other process-hungry computing applications), and there are plenty of consumptive and passively recreational ways it can use significantly more.  This will always be a question, depending on context.  And if we have the awareness to situate AI practices in the context of their energy consumption, their benefits, and their problems (e.g. in the models), then it is perhaps a tool we will use well.  However, given energy consumption and pollution is perhaps one of the key themes of the forseeable future -- for any tool, which promises benefit at such a great cost of energy -- the question of, "is this worth the energy?", will be the question that we repeat and reexamine and continue asking, just as we'd examine the context of using any other tool.<br>
+        Today, the main question seems to be, "is it a good use of energy."  That is a complicated and important question.  There are ways AI can probably use less energy than current practices (e.g. one prompt compared to 10 Google searches, or one prompt compared to other process-hungry computing applications), and there are plenty of consumptive and passively recreational ways it can use significantly more.  This will always be a question, depending on context.  And if we have the awareness to situate AI practices in the context of their energy consumption, their benefits, and their problems (e.g. in the models), then it is perhaps a tool we will use well.  However, given energy consumption and pollution is perhaps one of the key themes of the forseeable future -- for any tool, which promises benefit at such a great cost of energy -- the question of, "is this worth the energy?", will be the question that we repeat and reexamine and continue asking, just as we'd examine the context of using any other tool.<br> -->
 <!--         
         If we are using AI Art to "count how many angels can dance on the pin of a needle" -- passively consuming and entertaining ourselves with the pretty pictures that the screen makes, then it probably isn't a  Generally, if we're consuming it as a form of entertainment, we should just remember -- this form of entertainment uses a lot more energy than your mind, hanging out with friends, reading a book, and other ways to entertain yourself.  If you're using it in a generative way, and perhaps replacing other energy intensive practices in the process (e.g. power-hungry visual applications, extensive web searches), then perhaps it is a good use of energy, if you were going to use all that energy with the other practices anyway.  This will always be a question, and it will always be a depends. -->
         </div>
