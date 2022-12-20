a0e1062fc6bf0f437a2fb8c3a2089253deeb27c0
Sam Butler <hello@sambutler.us>
2022-12-20 17:00:34 +0100

    add air filter build and design

---
+++ src/routes/covid-filters.svelte
@@ -0,0 +1,9 @@
+<article class="h-entry">
+    <h1 class="p-name"><a class="u-url" href="/html-as-post">COVID Air Filters</a></h1>
+    <p style="font-style: italic">Sam Butler (December 20th, 2022 at 4:54pm CEST)</p>
+    <div class="e-content">   
+        <img src="./static/air-filter-build-compressed.jpeg" style="width: 100%;">
+        <img src="./static/air-filter-design-compressed.jpeg" style="width: 100%;">
+        ^ 2 filters stacked vertical on each wide side (4 total filters), 3 fans vertically arranged on each narrow side (6 total fans)
+        </div>
+        </article>
