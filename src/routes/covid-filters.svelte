6bcb8ce94e84552900d127b77c483ed6f2ff6788
Sam Butler <hello@sambutler.us>
2022-12-22 10:21:57 +0100

    update covid filters page and designs

--- src/routes/covid-filters.svelte
+++ src/routes/covid-filters.svelte
@@ -5,7 +5,7 @@
         <img src="./static/air-filter-build-compressed.jpeg" style="width: 100%;">
         <img src="./static/air-filter-design-compressed.jpeg" style="width: 100%;">
         ^ 2 filters stacked vertical on each wide side (4 total filters), 3 fans vertically arranged on each narrow side (6 total fans)
-        <img src="./static/10-filter-design.jpeg" style="width: 100%;">
+        <img src="./static/10-filter-design.jpg" style="width: 100%;">
         ^ 2 filters stacked vertical on each wide side (4 total filters), 5 fans vertically arranged on each narrow side (10 total fans)
 
         10-fan operation or 5-fan operation, using daisychain connector at top. Can also regulate through voltage.
@@ -13,5 +13,10 @@
         Daisychain for left-tower fans to right-tower fans seen in cord at the top.
 
         When one tower is not operational, can add a shroud to cover any air leaks, essentially converting it into a 5-filter fan.
+
+        Here are specific designs and measureents
+        <img src="./static/10-fan-vertical.png" style="width: 100%;">
+        <img src="./static/10-fan-wide-closeup.png" style="width: 100%;">
+        <img src="./static/10-fan-wide.png" style="width: 100%;">
         </div>
         </article>
