915cbc674ce0a6927cfc37ca6561c2179afa8f7e
Sam Butler <hello@sambutler.us>
2022-12-22 11:20:05 +0100

    Fix covid air filters page

--- src/routes/covid-filters.svelte
+++ src/routes/covid-filters.svelte
@@ -2,10 +2,10 @@
     <h1 class="p-name"><a class="u-url" href="/html-as-post">COVID Air Filters</a></h1>
     <p style="font-style: italic">Sam Butler (December 20th, 2022 at 4:54pm CEST)</p>
     <div class="e-content">   
-        <img src="./static/air-filter-build-compressed.jpeg" style="width: 100%;">
-        <img src="./static/air-filter-design-compressed.jpeg" style="width: 100%;">
+        <img src="./air-filter-build-compressed.jpeg" style="width: 100%;">
+        <img src="./air-filter-design-compressed.jpeg" style="width: 100%;">
         ^ 2 filters stacked vertical on each wide side (4 total filters), 3 fans vertically arranged on each narrow side (6 total fans)
-        <img src="./static/10-filter-design.jpg" style="width: 100%;">
+        <img src="./10-filter-design.jpg" style="width: 100%;">
         ^ 2 filters stacked vertical on each wide side (4 total filters), 5 fans vertically arranged on each narrow side (10 total fans)
 
         10-fan operation or 5-fan operation, using daisychain connector at top. Can also regulate through voltage.
@@ -15,8 +15,8 @@
         When one tower is not operational, can add a shroud to cover any air leaks, essentially converting it into a 5-filter fan.
 
         Here are specific designs and measureents
-        <img src="./static/10-fan-vertical-2.jpg" style="width: 100%;">
-        <img src="./static/10-fan-wide-closeup.png" style="width: 100%;">
-        <img src="./static/10-fan-wide.png" style="width: 100%;">
+        <img src="./10-fan-vertical-2.jpg" style="width: 100%;">
+        <img src="./10-fan-wide-closeup.png" style="width: 100%;">
+        <img src="./10-fan-wide.png" style="width: 100%;">
         </div>
         </article>
