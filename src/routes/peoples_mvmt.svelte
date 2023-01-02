5884fabe95bb19df8438e151f0fd07d8d0668242
Sam Butler <hello@sambutler.us>
2023-01-02 15:57:52 +0100

    adding doc

---
+++ src/routes/peoples_mvmt.svelte
@@ -0,0 +1,19 @@
+1. A collective, international front — of laborers, farmers, nurses, parents and caretakers builders and architects, workers and technicians, couriers and municipal workers — leading localization efforts (centered around agroecology and local food sovereignty, and local worker/community-owned industries and services), pushing for demilitarization and a transition to peacetime economies, pushing for climate debt payments from rich/imperialist/colonial countries to the countries they have oppressed and kept poor, pushing for land back and agrarian reform.
+
+2. Growing agroecological food in local foodsheds, mapped over local watersheds.
+
+3. Demanding demilitarization, transition to peacetime economies, and the conversion of existing military industrial bases into factories that export technologies for transitioning worlds.
+
+4. Demanding land back, property reform, the honoring of treaty rights, land to those that work it, and universal housing — paid for by the state, as cities like Vienna and Singapore have done.
+
+5. All of the things we need, we get automatically from living in a community — doing so in a way that does not damage the non-human environment, that uses locally sourced and stewarded materials, that provides fair wages and ownership to workers and community, including to those who provide domestic work and all others whose work and contributions are ignored in the capitalist budgets.
+
+This can begin with worker and community-led cooperative efforts, to produce and provision the things we need and the services we wish for — starting with food, wherever we are  — and aligned with internationalist and ecological values.  It can likewise start with the localization of materials, services, landscape management (which provides local material inputs), and the transition of existing services into worker and community-owned services.  Transition what we have already to worker and community ownership, and build what we need with worker and community ownership — all with local production and inputs, and international and ecological values.  Transition our economic activity to these local circuits.  Fight and build.  Struggle and choice.
+
+Want to join the front, and start making this real — locally and internationally?  You can join this groups.io instance (https://groups.io/g/peoples/join) so we can start communicating and collaborating.  Know other groups/people who would want to join?  You can invite/add them to the group once you join.  (This is an initial step to help communicate and collaborate — we can build and design other ways to communicate and collaborate as well.)
+
+📌 Join: https://groups.io/g/peoples/join
+
+This is in the spirit of the People's Agreement of Cochabamba (2010), and building on that, Max Ajl's "A People's Green New Deal".  More details on that below.
+
+More details on the People's Agreement, Max Ajl's "A People's Green New Deal", and related notes and excerpts: https://rebrand.ly/peoples-mvmt
