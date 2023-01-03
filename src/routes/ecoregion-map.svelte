6a0917238d09a176139d7c23e08add08cfa768d9
Sam Butler <hello@sambutler.us>
2023-01-03 10:03:24 +0100

    text for ecoregion map

---
+++ src/routes/ecoregion-map.svelte
@@ -0,0 +1,18 @@
+A map to show ecoregions (supersets of watersheds — with common plants, animals, climactic conditions, and human settlement patterns)?
+
+An ecoregion is "an area with characteristic flora, fauna, and climatic conditions, and related human settlement patterns, and can be comprised of several different watersheds, if the plants and animals are similar."  (More on [watersheds, ecoregions, and bioregions](https://deptofbioregion.org/bioregions/ecoregions-watersheds/)).
+
+This is useful to organizing and planning in ways that are more ecologically focused (as opposed to relying on administrative demarcations).  Also, from the experience of the Cascadia Bioregion in the PNW, many people are interested in seeing their local ecoregions — and don't quite know where to begin or find it, as this may require more technical mapmaking and natural science experience.
+
+Given that ecoregions are a superset of watersheds, and @mheberger's excellent [watershed tool ](https://mghydro.com/watersheds/), would an ecoregional map project interest you?  This could take many different forms.  It could simply be an image of a map/shapefiles, which illustrates ecoregions around the world.  That shapefile likewise be a layer in online interactive maps, including a layer in the [Global Watershed map](https://mghydro.com/watersheds/).  It could likewise be an interactive map, which allows you to traverse different scales — from local watersheds, to wider ecoregions, to widest bioregions ("bioregions: the full extent of watersheds within an interconnected area.  Bioregional borders tend to be jagged, and hard, such as mountain ranges, peaks, ridges, volcanoes, continental uplifts, tectonic plates and faults, defining how energy flows within a set of boundaries.")
+
+As I write this, it does indeed seem that analytical work to _identify_ ecoregions and bioregions could come first — that analysis could be rendered into shapefiles, which could then easily be integrated into interactive online maps etc.
+
+At the same time, perhaps there could be online tools and data to help bootstrap and assist with that analysis.  For example, taking into account the different characteristics relevant to ecoregions:
+
+- Watersheds: (https://mghydro.com/watersheds/)
+- Flora: the [PlantNet API](https://identify.plantnet.org/prediction) and [AI GeoSpecies Service](https://cos4cloud-eosc.eu/services/ai-geospecies/), which predict local flora based on a given location.
+- Fauna: (not sure about tools for this)
+- Climactic Conditions, Geology, Soil composition, Rainfall patterns: There are several online datasets and APIs for this data
+- Human Settlement Patterns: Perhaps census-related APIs, like [the American Community Survey and other US Census APIs](https://www.census.gov/data/developers/data-sets.html)?  Human cultures and ways of living may also play a role here (see [ecoregions of Cascadia](https://deptofbiorestg.wpengine.com/wp-content/uploads/2021/09/cascadia75ecoregionsnamedmap-1.jpg), which could require a more anthropological/sociological touch for that.)
+
