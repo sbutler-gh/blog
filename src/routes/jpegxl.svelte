0b9b5ca7c1a79ad06740ba5ececcc46096036d97
Sam Butler <hello@sambutler.us>
2023-01-04 19:29:56 +0100

    add jpegxl post

---
+++ src/routes/jpegxl.svelte
@@ -0,0 +1,3 @@
+Has there been any though to making a Chromium fork (or another browser fork) that is compatible with JPEG XL? @jxl
+
+It could also open a door to some other interesting browser tweaks (e.g. LibRedirect https://github.com/libredirect by default, HTMX https://htmx.org/ compatibility?)  And yet, at it's simplest, it could start with a simple browser fork that is compatible with JPEG XL?
