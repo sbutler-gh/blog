5b4c550895bbd5526ca7e6291d25835b21f66c3d
Sam Butler <hello@sambutler.us>
2023-01-31 16:03:02 +0100

    mowcart meme page

---
+++ src/routes/mowcart.svelte
@@ -0,0 +1,6 @@
+<img src="./growgrassridemowcart.png" style="width: 100%;">
+<img src="./feedsheepridemowcart.png" style="width: 100%;">
+
+mowcart memees, based on lawn mower conversions to go karts, and growing gardens and feeding sheep instead of mowing lawns
+
+mon jan 30th 2023, tues jan 31st 2023
