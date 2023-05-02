29a613d575800c66a87efeed785e55005b360579
Sam Butler <hello@sambutler.us>
2023-05-02 20:19:18 +0200

    Create climate-disobedience-civil-resistance-solutions-already-here.svelte

---
+++ src/routes/climate-disobedience-civil-resistance-solutions-already-here.svelte
@@ -0,0 +1,29 @@
+Imagine if every place in the Global North saw itself for what it is: a frontline community.
+
+Recognizing impending food shortages, fires, pollution, floods, and wet bulbs as happening here and now.
+
+And recognizing that survival through colonization/extraction still kills us — drowning us, starving us, and burning our lungs (just slower.)
+
+So we're left with a frontline struggle, wherever we call home, based on: autonomy and self-sufficiency, growing food, adapting to heat, ending pollution, stopping the existing/planned projects that are killing us, and building the worlds we want, while determining our own futures and histories.
+
+How might disobedience and direct action look differently?
+
+Growing food rebelliously in cities. Breaking down fences and building over enclosures. Blocking cars from our streets (forever), coordinating bus services, turning buildings to homes, rehabilitating wastelands of parking lots and lawns, organizing healthcare + education, governing autonomously in the face of armed governments
+
+Doing this in thousands of places, per Peter Gelderloos' "The Solutions Are Already Here."
+
+Building and Fighting, in the words of Cooperation Jackson.
+
+People and worker power at every step (Max Ajl, "A People's Green New Deal")
+
+What if climate disobedience and civil resistance looked like this — frontline resistance, as affected communities?
+
+Not expecting politicians or politics to help us survive (UK climate emergency? US climate bill? Paris accords?  Pleas from the UN general?)
+
+Even FDR's New Deal forced Indigenous land removal (Nick Estes, "Our History Is the Future") — and laid foundations of roads and highways, which are choking us today.
+
+Rather, refusing the death projects and extraction and genocides and suicide pacts embedded in our trajectories or todays.
+
+And building a transition in the only viable way in the world we're in — decoupled from industry, embodied emissions, excess construction and capitalism, and based on local ingenuity and renewal, survival and solidarity (internationally, and across races and differences in our neighborhoods) and acting as if the crisis is here already.
+
+What if climate disobedience and direct action starts with food?  To feed us, while we take back land and spaces, and mend our worlds for our survival?
