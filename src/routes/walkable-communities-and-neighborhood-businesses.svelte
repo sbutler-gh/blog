1dfad3ed8a0f8c4825566692f95498e86fea6b04
sbutler-gh <sam@ysli.be>
2023-10-30 12:42:18 -0400

    add walkable communities piece

---
+++ src/routes/walkable-communities-and-neighborhood-businesses.svelte
@@ -0,0 +1,95 @@
+<script>
+    import PlainSubscribe from "/src/lib/PlainSubscribe.svelte";
+    import ShareButton from "/src/lib/ShareButton.svelte";
+    import TopMatter from "/src/lib/TopMatter.svelte";  
+    
+    let title = "Front-Yard, Driveway and Neighborhood Businesses";
+    let description = "Getting to walkable communities overnight — and ending car dependency";
+    let og_image = "pgnd-cover.jpeg";
+    let local_url = "peoples-green-new-deal-summary-max-ajl"
+    let domain = "https://sambutler.us"
+    let full_url = {domain} + "/" + {local_url};
+  </script>
+
+  <style>
+    h4 > span {
+      font-weight: normal;
+    }
+
+    /* h4 {
+      display: inline;
+    } */
+
+    h4 + p {
+      display: inline;
+    }
+
+    /* previous:has(+ p){
+      display: inline;
+    } */
+
+  </style>
+  
+  <TopMatter title={title} description={description} og_image={og_image} full_url={full_url} local_url={local_url} domain={domain}></TopMatter>
+  <article class="h-entry">
+    <h1 class="p-name"><a class="u-url" href={local_url}>{title}</a></h1>
+    <!-- <p style="font-style: italic">Sam Butler (February 2ndth, 2022 at 11:45am CEST)</p> -->
+    <div class="e-content">
+          
+              <p><strong>We have a right to daily services, goods, and food -&mdash; without needing to get in a car.</strong></p>
+
+              <p>We have to reduce emissions as quickly as possible.<p>
+
+                <br>
+                <span class="text-gray-500 text-sm" style="width: max-content; font-size: 14px; color: gray; text-align: center; display: block; margin: auto;">Recieve future content like this
+                    <svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: bottom;" class="icon icon-tabler icon-tabler-mail" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <rect x="3" y="5" width="18" height="14" rx="2" />
+                    <polyline points="3 7 12 13 21 7" />
+                  </svg></span>
+                      <PlainSubscribe></PlainSubscribe>
+
+                      <br>
+
+              <p>One of our root causes is car dependency.&nbsp; Communities required to drive miles to a grocery store to get daily food and goods is a violation of our rights to a livable environment, because it implicitly locks us into a system which is destroying our climate through unnecessary emissions and pollution.&nbsp; Electric vehicles do not help in this respect, because:<strong> a)</strong><span style="font-weight: 400;"> when consumers switch to electric vehicles, their old cars continue emitting through new owners, </span><strong>b)</strong><span style="font-weight: 400;"> electric vehicle production is also in violation of our rights to a livable climate environment through environmental degradation and industrial emissions, and </span><strong>c) </strong><span style="font-weight: 400;">electric cars and all cars create significant dangerous pollution through tire particulates, which poisons our surrounding environment and causes cancer in humans, and further violates our rights to a livable climate and environment.<p>
+
+              <p>How can we overcome car dependency, and move on from a system where emissions and deadly pollution is required for everyday life &mdash; towards our rights to a livable climate?<p>
+
+              <p><strong>Front-yard, driveway, and neighborhood businesses.</strong></p>
+
+              <p>Grocery stores, cafes, school supply shops, general stores, repair clinics, pharmacies &mdash; right on our streets, in residential zones.&nbsp; Operating from a neighbors&rsquo; driveway, garage, or at a stand in their front-yard.<p>
+
+              <p>This would <strong>pre-empt</strong><span style="font-weight: 400;"> our need for cars anywhere.&nbsp; All of the places that are just houses and concrete and car storage &mdash; could become walkable and vibrant and low-emissions and minimal pollution overnight.<p>
+
+              <p>And indeed, these types of businesses are common everywhere in the world, including a select few areas of the US and Canada.<p>
+
+              <p>I called my local zoning board to talk about this, and they said it would almost certainly be illegal to have a front-yard/driveway/garage business in the municipality.&nbsp; Even if the municipality made an exception, they advised such an operation would likely run into state law on building codes.&nbsp; (They brought up lemonade stands and yard sales voluntarily to encourage that type of activity, but became very evasive when asked if those types of businesses are in standing with code or not.)<p>
+
+              <p>If we think about rapid transitions to lower emissions, at the scale of our crisis &mdash; this is the only thing I can imagine doing what we need to do.&nbsp; It is also the first step.&nbsp; Before we have businesses and essential services in the places we live, right down our street, anything we&rsquo;re building on top of that may be unnecessary.&nbsp; It&rsquo;s a path dependency issue.&nbsp; Allowing people to live within reach of essential services and goods is the foundation, which could allow us to rapidly transform and localize, </span><strong><em>without any further infrastructure build or funding.</em></strong></p>
+              <p>This could also help address pollution from packaging and containers, if people can get goods and re-fill them in the places the live &mdash; and live less transient, more local ways of life.<p>
+
+              <p>We have a right to a livable future and environment &mdash;&nbsp;and that means we have a right to access the daily services and products we need, without destroying our climate and environment in the process.&nbsp; We can do this by simply changing the zoning codes &mdash; and removing the codes that make this illegal.<p>
+
+              <p>What could reform look like, in terms of local codes?&nbsp; (The following is largely from </span><em><span style="font-weight: 400;">Strong Towns</span></em><span style="font-weight: 400;"> material)<p>
+              <p></p>
+              <ol>
+              <li style="font-weight: 400;"><strong>Legalize small</strong><span style="font-weight: 400;">.&nbsp; Allow people to start small with front-yard and driveway and garage businesses.</span><span style="font-weight: 400;"></span></li>
+              <ol>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Codes allowing for businesses up to a certain amount of patrons per day.</span><span style="font-weight: 400;"></span></li>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Codes allowing for businesses up to a certain amount of staff members (whether employees or volunteers), as an indicator of the size of establishment.</span><span style="font-weight: 400;"></span></li>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Codes to address parking, e.g. requiring that businesses do not create new parking needs and pushing towards non-car patrons (because walkability is the point of all of this.&nbsp; Accessibility could be an exception)</span><span style="font-weight: 400;"></span></li>
+              </ol>
+              <li style="font-weight: 400;"><strong>Legalize fast.&nbsp; </strong><span style="font-weight: 400;">Lower the operational costs and hurdles to get small businesses like these started.</span><span style="font-weight: 400;"></span></li>
+              <ol>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Simplify and streamline the permitting process, so people starting small can get going.</span><span style="font-weight: 400;"></span></li>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Lower the fees involved to make it more accessible.</span><span style="font-weight: 400;"></span></li>
+              <li style="font-weight: 400;"><span style="font-weight: 400;">Ensure the codes themselves are understandable to someone with a high school-level education, as a baseline for accessibility and how they are written. (Strong Towns recommendation)</span></li>
+              </ol>
+              </ol>
+              <p></p>
+              <p>See more on </span><a href="https://www.linkedin.com/feed/update/urn:li:activity:7120823383306465281/"><span style="font-weight: 400;">LinkedIn</span></a><span style="font-weight: 400;"> / </span><a href="https://twitter.com/sambutlerUS"><span style="font-weight: 400;">Twitter</span></a><span style="font-weight: 400;">. You can also see videos from </span><em><span style="font-weight: 400;">About Here </span></em><span style="font-weight: 400;">that inspired this vision (</span><a href="https://www.youtube.com/watch?v=wzBL85kTwwo"><em><span style="font-weight: 400;">Why did we make front-yard businesses illegal?</span></em></a><span style="font-weight: 400;"> and </span><a href="https://www.youtube.com/watch?v=9YspRX7bbTM"><em><span style="font-weight: 400;">How to bring back front-yard businesses</span></em></a><span style="font-weight: 400;">), and an article from Strong Towns: </span><a href="https://www.strongtowns.org/journal/2022/8/16/where-are-all-our-front-yard-businesses"><em><span style="font-weight: 400;">Where Are All Our Front-Yard Businesses?</span></em></a></p>
+            </div>
+
+<ShareButton></ShareButton>
+
+    </article>
