18964ccdefbb827d3e984ccf960e34013a146f5a
sbutler-gh <sam@ysli.be>
2024-02-20 16:24:18 -0500

    fixed 2c in 2024

---
+++ src/lib/SetupMap.svelte
@@ -0,0 +1,37 @@
+<script>
+            function setupMap() {
+
+lat = 38.8903605;
+lng = -77.09900;
+
+document.getElementById("latlng").value = `(38.890360, -77.09900)`;
+
+
+var map = L.map('map').setView([lat, lng], 12);
+
+marker = L.marker([lat, lng]).addTo(map);
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+console.log(e);
+marker
+.setLatLng(e.latlng);
+
+document.getElementById("latlng").value = `(${e.latlng.lat.toFixed(5)}, ${e.latlng.lng.toFixed(5)})`;
+
+console.log(document.getElementById("latlng"))
+
+
+}
+
+map.on('click', onMapClick);
+}
+</script>
